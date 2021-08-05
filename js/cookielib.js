7a31a16833e9fbe1d22496b3c3ebee21d945f85a
Jonas Kohl <github@jonaskohl.de>
2021-08-06 00:53:48 +0200

    Added cookie message

---
+++ js/cookielib.js
@@ -0,0 +1,33 @@
+(function() {
+  function getCookie(name) {
+    var dc = document.cookie;
+    var prefix = name + "=";
+    var begin = dc.indexOf("; " + prefix);
+    if (begin === -1) {
+      begin = dc.indexOf(prefix);
+      if (begin !== 0)
+        return null;
+    } else {
+      begin += 2;
+      var end = document.cookie.indexOf(";", begin);
+      if (end === -1) {
+        end = dc.length;
+      }
+    }
+    return decodeURI(dc.substring(begin + prefix.length, end));
+  }
+
+  function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+
+  window.getCookie = function(name) {
+    return getCookie(name);
+  };
+  window.setCookie = function(name, value, days) {
+    return setCookie(name, value, days);
+  };
+}());
